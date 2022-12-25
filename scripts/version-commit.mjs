82bdf50ef786e28f4e14f62e62d0ab92308ee661
William So <polyipseity@gmail.com>
2022-12-25 23:44:57 +0800

    Configure template files
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ scripts/version-commit.mjs
@@ -0,0 +1,26 @@
+import { spawnSync } from "child_process"
+
+function check(ret) {
+	if (ret.error !== undefined) throw ret.error
+}
+function log(ret) {
+	console.log(ret.stdout)
+	console.error(ret.stderr)
+}
+
+let ret
+ret = spawnSync("git", ["tag", "--points-at"], { encoding: 'utf8', })
+check(ret)
+const tag = ret.stdout.split("\n", 1)[0]
+
+ret = spawnSync("git", ["tag", "--list", "--format=%(contents:subject)\n%(contents:body)", tag], { encoding: 'utf8' })
+check(ret)
+const tag_msg = ret.stdout.trim()
+
+ret = spawnSync("git", ["commit", "--amend", "--no-edit", "--gpg-sign", "--signoff"], { encoding: 'utf8', })
+log(ret)
+check(ret)
+
+ret = spawnSync("git", ["tag", "--sign", "--force", "--file=-", tag], { encoding: 'utf8', input: tag_msg, })
+log(ret)
+check(ret)
