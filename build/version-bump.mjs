28740bbd11d782f91d93bdc9095779b0ac873457
William So <polyipseity@gmail.com>
2022-12-27 13:04:14 +0800

    Replace 'utf8' with 'utf-8'
    Signed-off-by: William So <polyipseity@gmail.com>

--- build/version-bump.mjs
+++ build/version-bump.mjs
@@ -7,15 +7,15 @@ const manifest_map = {
 	"description": "description",
 	"author": "author",
 }
-const pack = JSON.parse(readFileSync("package.json", "utf8"))
+const pack = JSON.parse(readFileSync("package.json", "utf-8"))
 
 // read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"))
+let manifest = JSON.parse(readFileSync("manifest.json", "utf-8"))
 for (const [key, value] of Object.entries(manifest_map)) manifest[key] = pack[value]
 for (const [key, value] of Object.entries(pack.obsidian)) manifest[key] = value
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"))
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"))
+let versions = JSON.parse(readFileSync("versions.json", "utf-8"))
 versions[manifest.version] = manifest.minAppVersion
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"))
--- build/version-commit.mjs
+++ build/version-commit.mjs
@@ -9,18 +9,18 @@ function log(ret) {
 }
 
 let ret
-ret = spawnSync("git", ["tag", "--points-at"], { encoding: "utf8", })
+ret = spawnSync("git", ["tag", "--points-at"], { encoding: "utf-8", })
 check(ret)
 const tag = ret.stdout.split("\n", 1)[0]
 
-ret = spawnSync("git", ["tag", "--list", "--format=%(contents:subject)\n%(contents:body)", tag], { encoding: "utf8" })
+ret = spawnSync("git", ["tag", "--list", "--format=%(contents:subject)\n%(contents:body)", tag], { encoding: "utf-8" })
 check(ret)
 const tag_msg = ret.stdout.trim()
 
-ret = spawnSync("git", ["commit", "--amend", "--no-edit", "--gpg-sign", "--signoff"], { encoding: "utf8", })
+ret = spawnSync("git", ["commit", "--amend", "--no-edit", "--gpg-sign", "--signoff"], { encoding: "utf-8", })
 log(ret)
 check(ret)
 
-ret = spawnSync("git", ["tag", "--sign", "--force", "--file=-", tag], { encoding: "utf8", input: tag_msg, })
+ret = spawnSync("git", ["tag", "--sign", "--force", "--file=-", tag], { encoding: "utf-8", input: tag_msg, })
 log(ret)
 check(ret)
