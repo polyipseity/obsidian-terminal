2cc0ec0f706f97fbcca78142512b1437ad694225
William So <polyipseity@gmail.com>
2023-01-01 12:10:30 +0800

    Add `zh-Hans` translation
    Signed-off-by: William So <polyipseity@gmail.com>

--- assets/locales.ts
+++ assets/locales.ts
@@ -1,12 +1,15 @@
 import * as assets from "assets/locales/en/assets.json"
 import * as en from "assets/locales/en/translation.json"
 import * as languages from "assets/locales/en/languages.json"
+import * as zhHans from "assets/locales/zh-Hans/translation.json"
 import * as zhHant from "assets/locales/zh-Hant/translation.json"
 
 export const DEFAULT_NAMESPACE = "translation"
 export const DEFAULT_LANGUAGE = "en"
 export const FALLBACK_LANGUAGES = {
 	"default": [DEFAULT_LANGUAGE],
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	"zh-CN": ["zh-Hans", "zh", DEFAULT_LANGUAGE],
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	"zh-TW": ["zh-Hant", "zh", DEFAULT_LANGUAGE],
 } as const
@@ -15,6 +18,10 @@ export const RESOURCES = {
 		[DEFAULT_NAMESPACE]: en,
 		assets,
 		languages,
+	},
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	"zh-Hans": {
+		[DEFAULT_NAMESPACE]: zhHans,
 	},
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	"zh-Hant": {
