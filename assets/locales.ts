9786075da43c724c4f635a959a86dc988635d3dc
William So <polyipseity@gmail.com>
2023-01-01 11:49:54 +0800

    Add `zh-Hant` translation
    Signed-off-by: William So <polyipseity@gmail.com>

--- assets/locales.ts
+++ assets/locales.ts
@@ -1,16 +1,23 @@
 import * as assets from "assets/locales/en/assets.json"
 import * as en from "assets/locales/en/translation.json"
 import * as languages from "assets/locales/en/languages.json"
+import * as zhHant from "assets/locales/zh-Hant/translation.json"
 
 export const DEFAULT_NAMESPACE = "translation"
 export const DEFAULT_LANGUAGE = "en"
 export const FALLBACK_LANGUAGES = {
 	"default": [DEFAULT_LANGUAGE],
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	"zh-TW": ["zh-Hant", "zh", DEFAULT_LANGUAGE],
 } as const
 export const RESOURCES = {
 	en: {
 		[DEFAULT_NAMESPACE]: en,
 		assets,
 		languages,
+	},
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	"zh-Hant": {
+		[DEFAULT_NAMESPACE]: zhHant,
 	},
 } as const
