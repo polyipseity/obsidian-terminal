cf863f1e76542d0fd0c2f309543014a8c40a904f
William So <polyipseity@gmail.com>
2023-02-10 14:47:30 +0800

    Create `importable()`
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/imports.ts
+++ sources/imports.ts
@@ -6,13 +6,24 @@ const
 		tmp: (): unknown => require("tmp"),
 	} as const),
 	MODULES = typedKeys<["tmp"]>()(BUNDLE)
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function dynamicRequire<T>(module: string): Promise<T> {
+	return dynamicRequireSync(module) as T
+}
+
 export function dynamicRequireSync(module: string): unknown {
 	if (inSet(MODULES, module)) {
 		return BUNDLE[module]()
 	}
 	return require(module)
 }
-// eslint-disable-next-line @typescript-eslint/require-await
-export async function dynamicRequire<T>(module: string): Promise<T> {
-	return dynamicRequireSync(module) as T
+
+export function importable(module: string): boolean {
+	try {
+		dynamicRequireSync(module)
+		return true
+	} catch {
+		return false
+	}
 }
