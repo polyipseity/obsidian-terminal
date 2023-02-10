bd00db71151ff5b66382196afba6a7af6a711725
William So <polyipseity@gmail.com>
2023-02-10 14:44:53 +0800

    Improve `bundle.ts`
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/bundle.ts
+++ sources/bundle.ts
@@ -6,11 +6,13 @@ const
 		tmp: (): unknown => require("tmp"),
 	} as const),
 	MODULES = typedKeys<["tmp"]>()(BUNDLE)
+export function dynamicRequireSync(module: string): unknown {
+	if (inSet(MODULES, module)) {
+		return BUNDLE[module]()
+	}
+	return require(module)
+}
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function dynamicRequire<T>(module: string): Promise<T> {
-	if (inSet(MODULES, module)) {
-		return BUNDLE[module]() as T
-	}
-	// eslint-disable-next-line @typescript-eslint/no-var-requires
-	return require(module) as T
+	return dynamicRequireSync(module) as T
 }
