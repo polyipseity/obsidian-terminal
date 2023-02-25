e70081c7ffcd8baf4d4da70a7be530bdbdd558bd
William So <polyipseity@gmail.com>
2023-02-25 20:12:31 +0800

    Compat constants
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/terminal/util.ts
+++ sources/terminal/util.ts
@@ -19,33 +19,34 @@ export const ESCAPE_SEQUENCE_INTRODUCER = "\u001b"
 const ESC = ESCAPE_SEQUENCE_INTRODUCER
 export const CONTROL_SEQUENCE_INTRODUCER = `${ESC}[`
 const CSI = CONTROL_SEQUENCE_INTRODUCER
-export const FUNCTION_IDENTIFIER_PREFIXES =
-	rangeCodePoint(codePoint("\x3c"), codePoint("\x3f"))
-export const FUNCTION_IDENTIFIER_INTERMEDIATES =
-	rangeCodePoint(codePoint("\x20"), codePoint("\x2f"))
-export const FUNCTION_IDENTIFIER_FINAL = deepFreeze({
-	"long": rangeCodePoint(codePoint("\x30"), codePoint("\x7e")),
-	"short": rangeCodePoint(codePoint("\x40"), codePoint("\x7e")),
-} as const)
-export const ALL_CSI_IDENTIFIERS = deepFreeze(cartesianProduct(
-	FUNCTION_IDENTIFIER_PREFIXES,
-	FUNCTION_IDENTIFIER_INTERMEDIATES,
-	FUNCTION_IDENTIFIER_INTERMEDIATES,
-	FUNCTION_IDENTIFIER_FINAL.short,
-).map(([prefix, intermediates0, intermediates1, final]) => ({
-	final,
-	intermediates: `${intermediates0}${intermediates1}`,
-	prefix,
-} as const satisfies IFunctionIdentifier)))
-export const ALL_DCS_IDENTIFIERS = ALL_CSI_IDENTIFIERS
-export const ALL_ESC_IDENTIFIERS = deepFreeze(cartesianProduct(
-	FUNCTION_IDENTIFIER_INTERMEDIATES,
-	FUNCTION_IDENTIFIER_INTERMEDIATES,
-	FUNCTION_IDENTIFIER_FINAL.long,
-).map(([intermediates0, intermediates1, final]) => ({
-	final,
-	intermediates: `${intermediates0}${intermediates1}`,
-} as const satisfies IFunctionIdentifier)))
+export const
+	FUNCTION_IDENTIFIER_PREFIXES =
+		rangeCodePoint(codePoint("\x3c"), codePoint("\x3f")),
+	FUNCTION_IDENTIFIER_INTERMEDIATES =
+		rangeCodePoint(codePoint("\x20"), codePoint("\x2f")),
+	FUNCTION_IDENTIFIER_FINAL = deepFreeze({
+		"long": rangeCodePoint(codePoint("\x30"), codePoint("\x7e")),
+		"short": rangeCodePoint(codePoint("\x40"), codePoint("\x7e")),
+	} as const),
+	ALL_CSI_IDENTIFIERS = deepFreeze(cartesianProduct(
+		FUNCTION_IDENTIFIER_PREFIXES,
+		FUNCTION_IDENTIFIER_INTERMEDIATES,
+		FUNCTION_IDENTIFIER_INTERMEDIATES,
+		FUNCTION_IDENTIFIER_FINAL.short,
+	).map(([prefix, intermediates0, intermediates1, final]) => ({
+		final,
+		intermediates: `${intermediates0}${intermediates1}`,
+		prefix,
+	} as const satisfies IFunctionIdentifier))),
+	ALL_DCS_IDENTIFIERS = ALL_CSI_IDENTIFIERS,
+	ALL_ESC_IDENTIFIERS = deepFreeze(cartesianProduct(
+		FUNCTION_IDENTIFIER_INTERMEDIATES,
+		FUNCTION_IDENTIFIER_INTERMEDIATES,
+		FUNCTION_IDENTIFIER_FINAL.long,
+	).map(([intermediates0, intermediates1, final]) => ({
+		final,
+		intermediates: `${intermediates0}${intermediates1}`,
+	} as const satisfies IFunctionIdentifier)))
 
 export function processText(text: string): string {
 	return text
