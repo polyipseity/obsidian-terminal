b2dfa2fb7b7d90718255aa3f462345e4a87bad48
William So <polyipseity@gmail.com>
2023-02-25 20:12:02 +0800

    Revert "Remove unused constants in `util.ts`"
    This reverts commit 128e9782de357d24da13980f2556609ce7cfd203.
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/terminal/util.ts
+++ sources/terminal/util.ts
@@ -1,17 +1,51 @@
 import {
 	type IDisposable,
+	type IFunctionIdentifier,
 	Terminal,
 	type ITerminalOptions as TerminalOptions,
 	type ITerminalInitOnlyOptions as TerminalOptionsInit,
 } from "xterm"
-import { deepFreeze, replaceAllRegex } from "sources/utils/util"
+import {
+	cartesianProduct,
+	deepFreeze,
+	rangeCodePoint,
+	replaceAllRegex,
+} from "sources/utils/util"
 import { isUndefined, range } from "lodash"
 import ansi from "ansi-escape-sequences"
+import { codePoint } from "sources/utils/types"
 
 export const ESCAPE_SEQUENCE_INTRODUCER = "\u001b"
 const ESC = ESCAPE_SEQUENCE_INTRODUCER
 export const CONTROL_SEQUENCE_INTRODUCER = `${ESC}[`
 const CSI = CONTROL_SEQUENCE_INTRODUCER
+export const FUNCTION_IDENTIFIER_PREFIXES =
+	rangeCodePoint(codePoint("\x3c"), codePoint("\x3f"))
+export const FUNCTION_IDENTIFIER_INTERMEDIATES =
+	rangeCodePoint(codePoint("\x20"), codePoint("\x2f"))
+export const FUNCTION_IDENTIFIER_FINAL = deepFreeze({
+	"long": rangeCodePoint(codePoint("\x30"), codePoint("\x7e")),
+	"short": rangeCodePoint(codePoint("\x40"), codePoint("\x7e")),
+} as const)
+export const ALL_CSI_IDENTIFIERS = deepFreeze(cartesianProduct(
+	FUNCTION_IDENTIFIER_PREFIXES,
+	FUNCTION_IDENTIFIER_INTERMEDIATES,
+	FUNCTION_IDENTIFIER_INTERMEDIATES,
+	FUNCTION_IDENTIFIER_FINAL.short,
+).map(([prefix, intermediates0, intermediates1, final]) => ({
+	final,
+	intermediates: `${intermediates0}${intermediates1}`,
+	prefix,
+} as const satisfies IFunctionIdentifier)))
+export const ALL_DCS_IDENTIFIERS = ALL_CSI_IDENTIFIERS
+export const ALL_ESC_IDENTIFIERS = deepFreeze(cartesianProduct(
+	FUNCTION_IDENTIFIER_INTERMEDIATES,
+	FUNCTION_IDENTIFIER_INTERMEDIATES,
+	FUNCTION_IDENTIFIER_FINAL.long,
+).map(([intermediates0, intermediates1, final]) => ({
+	final,
+	intermediates: `${intermediates0}${intermediates1}`,
+} as const satisfies IFunctionIdentifier)))
 
 export function processText(text: string): string {
 	return text
