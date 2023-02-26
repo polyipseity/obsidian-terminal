917e6f5aead377243041f59717d2f3c12aa7be18
William So <polyipseity@gmail.com>
2023-02-26 13:22:02 +0800

    Remove an unused package
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/@types/eastasianwidth.d.ts
+++
@@ -1,9 +0,0 @@
-module "eastasianwidth" {
-	declare namespace eastasianwidth {
-		function eastAsianWidth(character: string): string
-		function characterLength(character: string): number
-		function length(string: string): number
-		function slice(text: string, start = 0, end = 1): string
-	}
-	export = { ...eastasianwidth }
-}
