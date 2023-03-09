4c7634fd55b507c4396f0243b304f75ef612eb88
William So <polyipseity@gmail.com>
2023-03-09 16:19:33 +0800

    Parallelize `version-bump.mjs`
    Signed-off-by: William So <polyipseity@gmail.com>

--- build/version-bump.mjs
+++ build/version-bump.mjs
@@ -1,25 +1,39 @@
 import { readFile, writeFile } from "node:fs/promises"
 
-const manifestMap =
-{
-	author: "author",
-	description: "description",
-	version: "version",
-},
-	pack = JSON.parse(await readFile("package.json", "utf-8")),
+const MANIFEST_MAP =
+	Object.freeze({
+		author: "author",
+		description: "description",
+		version: "version",
+	}),
+	aPack = readFile("package.json", "utf-8").then(data => JSON.parse(data))
 
-	// Read minAppVersion from manifest.json and bump version to target version
-	manifest = JSON.parse(await readFile("manifest.json", "utf-8"))
-for (const [key, value] of Object.entries(manifestMap)) {
-	manifest[key] = pack[value]
-}
-for (const [key, value] of Object.entries(pack.obsidian)) {
-	manifest[key] = value
-}
-await writeFile("manifest.json", JSON.stringify(manifest, null, "\t"))
-
-// Update versions.json with target version and minAppVersion from manifest.json
-const versions = JSON.parse(await readFile("versions.json", "utf-8"))
-versions[manifest.version] = manifest.minAppVersion
-
-await writeFile("versions.json", JSON.stringify(versions, null, "\t"))
+await Promise.all([
+	writeFile(
+		"manifest.json",
+		JSON.stringify(await (async () => {
+			const manifest =
+				JSON.parse(await readFile("manifest.json", { encoding: "utf-8" })),
+				pack = await aPack
+			for (const [key, value] of Object.entries(MANIFEST_MAP)) {
+				manifest[key] = pack[value]
+			}
+			for (const [key, value] of Object.entries(pack.obsidian)) {
+				manifest[key] = value
+			}
+			return manifest
+		})(), null, "\t"),
+		{ encoding: "utf-8" },
+	),
+	writeFile(
+		"versions.json",
+		JSON.stringify(await (async () => {
+			const versions =
+				JSON.parse(await readFile("verisons.json", { encoding: "utf-8" })),
+				pack = await aPack
+			versions[pack.version] = pack.obsidian.minAppVersion
+			return versions
+		})(), null, "\t"),
+		{ encoding: "utf-8" },
+	),
+])
