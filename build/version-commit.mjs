1ea3fd3b326e44a1e2169fa0b0df684d9cb95b68
William So <polyipseity@gmail.com>
2023-03-11 00:42:48 +0800

    Remove newline from 2 files on version command
    Signed-off-by: William So <polyipseity@gmail.com>

--- build/version-commit.mjs
+++ build/version-commit.mjs
@@ -1,3 +1,4 @@
+import { readFile, writeFile } from "node:fs/promises"
 import { execFile } from "node:child_process"
 import { promisify } from "node:util"
 
@@ -14,17 +15,34 @@ async function run(...args) {
 	return stdout
 }
 
-const
-	tag = (await run(
-		"git",
-		["tag", "--points-at"],
-		{ encoding: "utf-8" },
-	)).split("\n", 1)[0].trim(),
-	tagMessage = (await run(
-		"git",
-		["tag", "--list", "--format=%(contents:subject)\n%(contents:body)", tag],
-		{ encoding: "utf-8" },
-	)).trim()
+const [{ tag, tagMessage }] = await Promise.all([
+	(async () => {
+		const tag0 = (await run(
+			"git",
+			["tag", "--points-at"],
+			{ encoding: "utf-8" },
+		)).split("\n", 1)[0].trim()
+		return {
+			tag: tag0,
+			tagMessage: (await run(
+				"git",
+				[
+					"tag",
+					"--list",
+					"--format=%(contents:subject)\n%(contents:body)",
+					tag0,
+				],
+				{ encoding: "utf-8" },
+			)).trim(),
+		}
+	})(),
+	...["package-lock.json", "package.json"]
+		.map(async file => writeFile(
+			file,
+			(await readFile(file, { encoding: "utf-8" })).trimEnd(),
+			{ encoding: "utf-8" },
+		)),
+])
 await run(
 	"git",
 	["commit", "--amend", "--no-edit", "--gpg-sign", "--signoff"],
