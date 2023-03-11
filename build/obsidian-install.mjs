d5a2f151e544f35709eaaefcc807ed610b3aad10
William So <polyipseity@gmail.com>
2023-03-11 10:02:00 +0800

    Make `obsidian-install.mjs` read from `manifest.json` to get the ID
    Signed-off-by: William So <polyipseity@gmail.com>

--- build/obsidian-install.mjs
+++ build/obsidian-install.mjs
@@ -1,9 +1,10 @@
-import { copyFile, mkdir } from "node:fs/promises"
+import { copyFile, mkdir, readFile } from "node:fs/promises"
 import { PATHS } from "./util.mjs"
 import process from "node:process"
 
 const ARGV_DESTINATION = 2,
-	DESTINATION_PREFIX = ".obsidian/plugins/terminal",
+	DESTINATION_PREFIX = `.obsidian/plugins/${JSON
+		.parse(await readFile(PATHS.manifest, { encoding: "utf-8" })).id}`,
 	DESTINATION = `${process.argv[ARGV_DESTINATION] ?? "."}/${DESTINATION_PREFIX}`
 
 await mkdir(DESTINATION, { recursive: true })
