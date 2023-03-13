53e147f84d3974be89a9491c7a7255d7b0361a2f
William So <polyipseity@gmail.com>
2023-03-13 12:59:58 +0800

    Modify imports in build scripts
    Signed-off-by: William So <polyipseity@gmail.com>

--- build/obsidian-install.mjs
+++ build/obsidian-install.mjs
@@ -1,11 +1,11 @@
 import { copyFile, mkdir, readFile } from "node:fs/promises"
 import { PATHS } from "./util.mjs"
-import process from "node:process"
+import { argv } from "node:process"
 
 const ARGV_DESTINATION = 2,
 	DESTINATION_PREFIX = `${PATHS.obsidianPlugins}/${JSON
 		.parse(await readFile(PATHS.manifest, { encoding: "utf-8" })).id}`,
-	DESTINATION = `${process.argv[ARGV_DESTINATION] ?? "."}/${DESTINATION_PREFIX}`
+	DESTINATION = `${argv[ARGV_DESTINATION] ?? "."}/${DESTINATION_PREFIX}`
 
 await mkdir(DESTINATION, { recursive: true })
 await Promise.all([PATHS.manifest, PATHS.main, PATHS.styles]
