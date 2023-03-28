f2a79e78d74032864f1a52e8a770aa9ec8dd152b
William So <polyipseity@gmail.com>
2023-03-28 11:43:24 +0800

    Improve async reading and writing in `version.mjs`
    Signed-off-by: William So <polyipseity@gmail.com>

--- build/version.mjs
+++ build/version.mjs
@@ -7,36 +7,31 @@ const MANIFEST_MAP =
 		description: "description",
 		version: "version",
 	}),
-	aPackage = readFile(PATHS.package, "utf-8").then(data => JSON.parse(data))
+	aPackage = readFile(PATHS.package, "utf-8").then(data => JSON.parse(data)),
+	aVersions = readFile(PATHS.versions, "utf-8").then(data => JSON.parse(data))
 
 await Promise.all([
-	(async () => writeFile(
-		PATHS.manifest,
-		JSON.stringify(await (async () => {
-			const manifest =
-				JSON.parse(await readFile(PATHS.manifest, { encoding: "utf-8" })),
-				pack = await aPackage
-			for (const [key, value] of Object.entries(MANIFEST_MAP)) {
-				manifest[key] = pack[value]
-			}
-			for (const [key, value] of Object.entries(pack.obsidian)) {
-				manifest[key] = value
-			}
-			return manifest
-		})(), null, "\t"),
-		{ encoding: "utf-8" },
-	))(),
-	(async () => writeFile(
-		PATHS.versions,
-		JSON.stringify(await (async () => {
-			const versions =
-				JSON.parse(await readFile(PATHS.versions, { encoding: "utf-8" })),
-				pack = await aPackage
-			versions[pack.version] = pack.obsidian.minAppVersion
-			return versions
-		})(), null, "\t"),
-		{ encoding: "utf-8" },
-	))(),
+	(async () => {
+		const pack = await aPackage
+		await writeFile(
+			PATHS.manifest,
+			JSON.stringify({
+				...Object.fromEntries(Object.entries(MANIFEST_MAP)
+					.map(([key, value]) => [key, pack[value]])),
+				...pack.obsidian,
+			}, null, "\t"),
+			{ encoding: "utf-8" },
+		)
+	})(),
+	(async () => {
+		const [versions, pack] = await Promise.all([aPackage, aVersions])
+		versions[pack.version] = pack.obsidian.minAppVersion
+		await writeFile(
+			PATHS.versions,
+			JSON.stringify(versions, null, "\t"),
+			{ encoding: "utf-8" },
+		)
+	})(),
 ])
 await execute(
 	"git",
