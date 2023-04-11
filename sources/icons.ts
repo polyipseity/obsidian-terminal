966bcb18a75285e7fffa4941fd369b8673b68448
William So <polyipseity@gmail.com>
2023-04-11 08:15:44 +0800

    Use icon `clipboard-paste` instead of `clipboard`
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/icons.ts
+++ sources/icons.ts
@@ -1,8 +1,20 @@
-import { type Plugin, addIcon } from "obsidian"
+import { ClipboardPaste, createElement } from "lucide"
+import { type Plugin, addIcon, getIcon } from "obsidian"
 import { siLinux, siMacos, siWindows } from "simple-icons"
 import { UnnamespacedID } from "./utils/obsidian"
 
 export function loadIcons(plugin: Plugin): void {
+	for (const [key, value] of Object.entries({
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		"clipboard-paste": ClipboardPaste,
+	})) {
+		if (!getIcon(key)) {
+			const icon = createElement(value)
+			icon.setAttribute("width", "100")
+			icon.setAttribute("height", "100")
+			addIcon(key, icon.outerHTML)
+		}
+	}
 	for (const [key, value] of Object.entries({
 		linux: siLinux,
 		macos: siMacos,
