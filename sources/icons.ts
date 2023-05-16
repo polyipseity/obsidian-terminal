a5d3b709f9123abe68d6d0b7e11154ad86cf1573
William So <polyipseity@gmail.com>
2023-05-17 00:42:27 +0800

    Use `PLACEHOLDERPlugin` in `icons.ts`
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/icons.ts
+++ sources/icons.ts
@@ -1,7 +1,8 @@
-import { type Plugin, getIcon } from "obsidian"
+import type { PLACEHOLDERPlugin } from "./main.js"
+import { getIcon } from "obsidian"
 import { registerLucideIcon } from "obsidian-plugin-library"
 
-export function loadIcons(context: Plugin): void {
+export function loadIcons(context: PLACEHOLDERPlugin): void {
 	for (const [key, value] of Object.entries<never>({})) {
 		if (getIcon(key)) {
 			self.console.warn(key)
