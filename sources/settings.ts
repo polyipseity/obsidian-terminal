a68b0e0793318ce0ff7775a61c4d06585fb3e029
William So <polyipseity@gmail.com>
2023-07-21 19:59:59 +0800

    Remove redundant settings title
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/settings.ts
+++ sources/settings.ts
@@ -24,7 +24,6 @@ export class SettingTab extends AdvancedSettingTab<Settings> {
 			docs,
 			ui,
 		} = this
-		this.newTitleWidget()
 		this.newDescriptionWidget()
 		this.newLanguageWidget(
 			Settings.DEFAULTABLE_LANGUAGES,
