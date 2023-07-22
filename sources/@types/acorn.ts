0286bccf1575dd43090fdaa47ee6c0abea6c35ee
William So <polyipseity@gmail.com>
2023-07-22 09:24:42 +0800

    Move module augmentations before the imports
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/@types/acorn.ts
+++ sources/@types/acorn.ts
@@ -1,6 +1,3 @@
-import type { Options as Options0 } from "acorn"
-import type estree from "estree"
-
 declare module "acorn" {
 	// https://github.com/acornjs/acorn/issues/1136#issuecomment-1203671368
 	type ExtendNode<T> =
@@ -10,5 +7,7 @@ declare module "acorn" {
 				end: number
 			} : unknown)
 		& { [K in keyof T]: T[K] extends object ? ExtendNode<T[K]> : T[K] }
-	function parse(s: string, o: Options0): ExtendNode<estree.Program>
+	function parse(s: string, o: Options): ExtendNode<estree.Program>
 }
+import type { Options } from "acorn"
+import type estree from "estree"
