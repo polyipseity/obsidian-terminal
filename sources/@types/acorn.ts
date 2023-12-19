1d4f116da7be90844afa55bb69a1763eb13441ee
William So <polyipseity@gmail.com>
2023-12-19 13:46:44 +0800

    Suppress odd tsc error
    Signed-off-by: William So <polyipseity@gmail.com>

--- sources/@types/acorn.ts
+++ sources/@types/acorn.ts
@@ -9,5 +9,7 @@ declare module "acorn" {
 		& { [K in keyof T]: T[K] extends object ? ExtendNode<T[K]> : T[K] }
 	function parse(s: string, o: Options): ExtendNode<estree.Program> & Node
 }
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore
 import type { Node, Options } from "acorn"
 import type estree from "estree"
