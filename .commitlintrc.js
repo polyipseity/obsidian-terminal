d588774c1a0b9503dc1c57a46aae41f888657da5
William So <polyipseity@gmail.com>
2026-01-31 15:53:36 +0800

    chore(commitlint): ignore dependabot signoff in lint
    - Update .commitlintrc.js to ignore commit messages containing 'Signed-off-by: dependabot[bot]'.
    - Prevents commitlint from blocking automated Dependabot PRs with signoff.
    
    Refs: commitlint config improvement

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,4 @@
 export default {
-  extends: ['@commitlint/config-conventional']
+	extends: ['@commitlint/config-conventional'],
+	ignores: [(message) => message.includes('Signed-off-by: dependabot[bot]')],
 };
